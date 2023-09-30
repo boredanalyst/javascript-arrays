7ed4d3a1dbd9694b897643ead82bf0807cdf996f
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:45:28 +0000

    Adding practice files.

---
+++ arrays/includes.js
@@ -0,0 +1,3 @@
+// you can use includes to see if an element is inside an array.
+const colors = ["red","orange","blue"];
+console.log(colors.includes("red")); // This will return true if the value is inside the array.
