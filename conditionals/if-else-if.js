667c2570c039904475a50856cf198d98da4b6eb1
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 11:45:17 +0000

    Added practice files.

---
+++ conditionals/if-else-if.js
@@ -0,0 +1,11 @@
+// We can string together multiple conditions based on else ifs.
+
+const age = 20;
+
+if (age > 30) {
+    console.log("You are too old.");
+} else if (age >= 18) {
+    console.log("You are of age.")
+} else {
+    console.log("You are too young!")
+}
