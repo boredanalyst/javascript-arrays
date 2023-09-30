df45a407b03a44ab5227f4699518847e221a53b0
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:46:01 +0000

    Adding practice files.

---
+++ arrays/split.js
@@ -0,0 +1,7 @@
+// You can split characters based on a delimiter to return an array.
+
+const fruits = "apple,banana,guava";
+
+const myFruits = fruits.split(','); // This will split the fruits variable using the ',' delimiter.
+
+console.log(myFruits);
