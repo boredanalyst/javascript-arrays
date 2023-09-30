3d02047f71a5d44b46a2faf6d289c9ec1d838e2b
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 11:45:36 +0000

    Added practice files.

---
+++ conditionals/not-equal.js
@@ -0,0 +1,9 @@
+// Checking if a value is NOT equal to something.
+
+const color = "red";
+
+if (color !== "red") {
+    console.log("My favorite color is not red.");
+} else {
+    console.log("My favorite color is red.");
+}
