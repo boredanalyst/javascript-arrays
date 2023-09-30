8aa8c9f30ceda2a926502b7921e931b39d861f8b
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:45:12 +0000

    Adding practice files.

---
+++ arrays/accessing-multidimensions.js
@@ -0,0 +1,3 @@
+// If you have arrays within arrays, you need two or more indicese to access these arrays elements.
+const colors = ["red","yellow","blue",["green","pink"]];
+console.log(colors[3][0]);
