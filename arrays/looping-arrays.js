41e6f8122dadad2e174e565868c47b304611a61a
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:45:41 +0000

    Adding practice files.

---
+++ arrays/looping-arrays.js
@@ -0,0 +1,7 @@
+// Manipulate all elements of an array using for loops.
+
+const colors = ["red","orange","yellow","green","blue","purple"];
+
+for (const color of colors) {
+    console.log(color.toUpperCase()); // This will return all colors in the array in upper case.
+}
