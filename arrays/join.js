1d840a4f5f7f7a62c6ea969dbd804ff923e96cfe
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:45:36 +0000

    Adding practice files.

---
+++ arrays/join.js
@@ -0,0 +1,7 @@
+// You can join elements of an array with .join();
+
+const colors = ["red","orange","green"];
+
+const myColors = colors.join(','); // This will concatenate all elements of the colors array with a comma.
+
+console.log(myColors);
