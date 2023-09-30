f0177bfb623c7d6d24acc2acdeee728d5ffc9c1a
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:46:03 +0000

    Adding practice files.

---
+++ arrays/toString.js
@@ -0,0 +1,7 @@
+// Convert arrays into strings with .toString()
+// This will always concatenate arrays with a comma.
+
+const colors = ["red","orange","yellow"];
+const myColors = colors.toString();
+
+console.log(myColors);
