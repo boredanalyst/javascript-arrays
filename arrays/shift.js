d17858b2df86b7b59da43bae4fd3034ea10845eb
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:45:55 +0000

    Adding practice files.

---
+++ arrays/shift.js
@@ -0,0 +1,8 @@
+// To remove the first item from an array, use .shift();
+
+const colors = ["red","orange","yellow"];
+
+console.log(colors.shift()); // This will return the first item of the array.
+
+// This will return only the items that are left.
+console.log(colors);
