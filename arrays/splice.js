f942d8690243955f0936c1b3e18789212eb44407
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:45:58 +0000

    Adding practice files.

---
+++ arrays/splice.js
@@ -0,0 +1,5 @@
+// You can remove a certain portion of the array.
+
+const colors = ["red","yellow","green","blue","indigo","purple"];
+
+console.log(colors.splice(1,3)); // This will return the elements occupying the indices 1 to 3.
