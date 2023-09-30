0c9b400b092e1b07c3d63992706e2be4c77d14eb
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:45:33 +0000

    Adding practice files.

---
+++ arrays/indexOf.js
@@ -0,0 +1,3 @@
+// You can find the index of a value in an array.
+const colors = ["red","blue","yellow"];
+console.log(colors.indexOf("blue")); // This returns the index location of the color blue in the array.
