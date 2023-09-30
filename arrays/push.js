fc17252e5fba143f2696cd3d37ee3c46f75bf3fe
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 18:45:53 +0000

    Adding practice files.

---
+++ arrays/push.js
@@ -0,0 +1,10 @@
+// You can add elements to an array using .push()
+
+const colors = ["red","orange","green"];
+
+colors.push("pink");
+console.log(colors);
+
+// You can also add multiple elements with one push.
+colors.push("yellow","gray");
+console.log(colors);
