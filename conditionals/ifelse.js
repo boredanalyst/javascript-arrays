5d746fb20a6d1cead0a2dae8c4c1badaba3108d9
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 11:45:27 +0000

    Added practice files.

---
+++ conditionals/ifelse.js
@@ -0,0 +1,9 @@
+// If...else statements decide outputs based on conditions.
+
+const age = 12;
+
+if (age > 18) {
+    console.log("You're too young!");
+} else {
+    console.log("You're too young.");
+}
