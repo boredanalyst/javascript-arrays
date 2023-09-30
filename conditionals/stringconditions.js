5b34e826865334d855b99b51075d10a11fe3648a
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-09-30 11:45:41 +0000

    Added practice files.

---
+++ conditionals/stringconditions.js
@@ -0,0 +1,9 @@
+// You can check conditions based on items.
+
+const color = "red";
+
+if (color.includes("a")) {
+    console.log("This color has the letter 'a'!");
+} else {
+    console.log("This color does not include the letter 'a'!")
+}
