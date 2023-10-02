d1b468a84e76770f6cff4c5048bee6c1e25509d2
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-10-02 15:36:20 +0000

    Adding new files.

---
+++ classes/basic.js
@@ -0,0 +1,10 @@
+class Car {
+    constructor(name, wheels) {
+        this.name = name;
+        this.wheels = wheels;
+    }
+}
+
+const honday = new Car("Honday",4);
+
+console.log(honday.name,honday.wheels);
