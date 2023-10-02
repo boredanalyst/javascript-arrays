e23bc41db121eabd3adeeadcd1cebdaa3a275747
Louis Christian Garcia <119511369+boredanalyst@users.noreply.github.com>
2023-10-02 15:36:35 +0000

    Adding new files.

---
+++ conditionals/switch.js
@@ -0,0 +1,17 @@
+let trafficLight = "pink";
+
+switch (trafficLight) {
+    case "red":
+        console.log("The light is red. Stop!");
+        break;
+    case "yellow":
+        console.log("The light is yellow. Slow down!");
+        break;
+    case "green":
+        console.log("The light is green. Go!");
+        break;
+    default:
+        console.log("The light is turned off.");
+        break;
+}
+
